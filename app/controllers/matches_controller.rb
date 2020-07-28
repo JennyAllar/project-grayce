cbf3ca5d86edf3239b5547d80710ae83192a3586
Jenny Allar <jenny.allar@techstars.com>
2020-07-27 23:39:09 -0600

    working app

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -10,5 +10,24 @@ class MatchesController < ApplicationController
   def new
     @experts = Expert.all
     @users = User.all
+    @match = Match.new
+  end
+
+  def create
+    #TODO: use private params method and hidden id field in form
+    @match = Match.new(user_id: params[:match][:user_id], expert_id: params[:match][:expert_id], id: (Match.count + 1))
+    if @match.save
+      flash[:notice] = "match saved"
+      redirect_to admin_path
+    else
+      flash[:error] = "failed to save match"
+      redirect_to new_match_path
+    end
+  end
+
+  def destroy
+    @match = Match.find(params[:id])
+    @match.destroy
+    redirect_to admin_path
   end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,6 @@
 class Match < ApplicationRecord
   belongs_to :expert
   belongs_to :user
+
+  validates_uniqueness_of :user_id, scope: :expert_id
 end
